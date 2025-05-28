810e98fbc19ce2585885a91ded3810fea340df08
lefv-server <luisv@lefv.io>
2025-05-27 22:43:00 -0400

    Fixing bad references.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import Home from "@/pages/Home";
 import Layout from "./components/layout/Layout";
+import { Toaster } from "./components/ui/toaster";
 
 function Router() {
   return (
